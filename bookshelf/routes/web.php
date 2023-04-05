7462d700237e7593e1329380bc1c75085793088c
RutaLengvinene <ruta.lengviniene@gmail.com>
2023-04-05 16:45:22 +0300

    creating table

--- bookshelf/routes/web.php
+++ bookshelf/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostController::class, 'index']);
+Route::get('/detail', [PostController::class, 'detailPost']);
+
+
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+//Route::get('/detail', function () {
+//    return view('detail');
+//});
